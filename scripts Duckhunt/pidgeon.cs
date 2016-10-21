c55352fc1af0e6f0824a777b704606689cd02075
tony aben <tony aben>
2016-10-21 15:36:56 +0200

    Add scripts

---
+++ scripts Duckhunt/pidgeon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApplication1.Properties;
+
+namespace WindowsFormsApplication1
+{
+    class Pidgeon : Target
+    {
+        public Pidgeon(int xCoord, int yCoord) : base(Resources.pidgeon2, xCoord, yCoord)
+        {
+
+        }
+    }
+}
